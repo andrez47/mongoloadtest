ae4e454923115b86dff476483df979f43e11670c
Andres Hernandez <andres@appcoast.com>
2011-02-24 14:41:22 -0600

    Seeds changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,32 +16,34 @@ p "Creating Members..."
   )
 
 }
+members = Member.all
 memberCount = Member.count - 1
 
 # create 15.000 horses
 p "Creating Horses..."
-15000.times {
+14500.times {
   Horse.create(:name => Bitgain::Mockdata::Names.project_name, :bio => Bitgain::Mockdata::Words.three, \
     :parelli_level => Bitgain::Mockdata::Numbers.random_between(1..10), :birthday => "Sat Dec 11 18:00:00 1991", \
-    :age => 20, :owner => Member.all[Bitgain::Mockdata::Numbers.random_between(0..memberCount)]
+    :age => 20, :owner => members[Bitgain::Mockdata::Numbers.random_between(0..memberCount)]
   )
 }
+horses = Horse.all
 horseCount = Horse.count - 1
 
 # create 200.000 posts
 p "Creating Posts..."
 0.times {
   post = Post.create(:body => Bitgain::Mockdata::Words.some(5..15), \
-    :writer => Member.all[Bitgain::Mockdata::Numbers.random_between(0..memberCount)], \
-    :member_recipient => Member.all[Bitgain::Mockdata::Numbers.random_between(0..memberCount)], \
-    :horse_partner => Horse.all[Bitgain::Mockdata::Numbers.random_between(0..horseCount)], \
-    :horse_recipient => Horse.all[Bitgain::Mockdata::Numbers.random_between(0..horseCount)]
+    :writer => members[Bitgain::Mockdata::Numbers.random_between(0..memberCount)], \
+    :member_recipient => members[Bitgain::Mockdata::Numbers.random_between(0..memberCount)], \
+    :horse_partner => horses[Bitgain::Mockdata::Numbers.random_between(0..horseCount)], \
+    :horse_recipient => horses[Bitgain::Mockdata::Numbers.random_between(0..horseCount)]
   )
   # create 1..15 comments
   ran = Bitgain::Mockdata::Numbers.random_between(1..15)
   ran.times {
     post.comments << Comment.new(:body => Bitgain::Mockdata::Words.some(5..15), \
-      :writer => Member.all[Bitgain::Mockdata::Numbers.random_between(0..memberCount)]
+      :writer => members[Bitgain::Mockdata::Numbers.random_between(0..memberCount)]
     )
   }
 }
